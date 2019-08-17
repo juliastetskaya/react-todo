e468eb372bccf2dc45b6018c6e9b51c69d55994c
juliastetskaya <juliastetskaya@gmail.com>
2019-08-18 00:03:57 +0300

    implement deleting todo list items

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import ItemStatusFilter from '../item-status-filter';
 import TodoList from '../todo-list';
 
-const App = () => {
-  const todos = [
+export default class App extends Component {
+  state = {
+    todos: [
     { label: 'Drink Coffee', important: false, id: 1 },
     { label: 'Make Awesome App', important: true, id: 2 },
     { label: 'Have a lunch', important: false, id: 3 },
-  ];
+    ]
+  }
 
-  return (
-    <div className='todo-app'>
-      <AppHeader toDo={3} done={1} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  removeItem = (removedId) => {
+    const { todos } = this.state;
+    const result = todos.filter(({ id }) => id !== removedId);
+    this.setState({ todos: result });
+  }
+  
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className='todo-app'>
+        <AppHeader toDo={3} done={1} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList todos={todos} onRemove={this.removeItem}/>
       </div>
-      <TodoList todos={todos} onRemove={(id) => console.log(id)}/>
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
