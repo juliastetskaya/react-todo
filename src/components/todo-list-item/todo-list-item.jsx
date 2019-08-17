16a8e2a660c39f62e00ce596c324e6b2a41fbb56
juliastetskaya <juliastetskaya@gmail.com>
2019-08-17 22:02:32 +0300

    redo two functional components to classes

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  }
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+export default class TodoListItem extends Component {
+
+  render() {
+    const { label, important = false } = this.props;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    }
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={style}>
+          {label}
+        </span>
+  
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right">
+            <i className="fa fa-exclamation" />
+        </button>
+  
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right">
+            <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
+    );
+  }
 };
-
-export default TodoListItem;
