4ae9713d6822c586cf937402f074aaad05829170
juliastetskaya <juliastetskaya@gmail.com>
2019-08-18 21:42:23 +0300

    add form for creating new items

--- src/components/ItemAddForm/item-add-form.jsx
+++ src/components/ItemAddForm/item-add-form.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
 export default class ItemAddForm extends Component {
-  render() {
+  state = {
+    label: '',
+  }
+
+  onChange = ({ target }) => {
+    this.setState({label: target.value});
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
     const { addItem } = this.props;
+    const { label } = this.state;
+    addItem(label);
+    this.setState({label: ''});
+  }
+
+
+  render() {
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary" onClick={() => addItem('Drink tea')}>Add Item</button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onChange}
+          placeholder="What needs to be done"
+          value={this.state.label} />
+        <button className="btn btn-outline-secondary">
+          Add Item
+        </button>
+      </form>
     )
   }
 };
