5c22f71c6a17f7cd19ebffc518eba77c3f8e6f91
juliastetskaya <juliastetskaya@gmail.com>
2019-08-20 14:55:48 +0300

    Add files via upload

---
+++ precache-manifest.4b573a1b186c6003cb9b96a87e02181e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ad9b98eed0907700fc90d95a75f74e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a85ca46ef3f13808a76",
+    "url": "./static/css/main.6d30557f.chunk.css"
+  },
+  {
+    "revision": "9a1c526577a1cad3d336",
+    "url": "./static/js/2.098f9a7a.chunk.js"
+  },
+  {
+    "revision": "7a85ca46ef3f13808a76",
+    "url": "./static/js/main.8673821e.chunk.js"
+  },
+  {
+    "revision": "d74572b337f05ece3564",
+    "url": "./static/js/runtime~main.3d8b850d.js"
+  }
+]);
